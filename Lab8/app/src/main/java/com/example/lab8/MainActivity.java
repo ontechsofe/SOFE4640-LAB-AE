7e8a3dc042219a4f89ed5748746e5a2e62b308b7
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-11-06 23:01:47 -0500

    Lab8

---
+++ Lab8/app/src/main/java/com/example/lab8/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lab8;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+
+    }
+
+
+}
